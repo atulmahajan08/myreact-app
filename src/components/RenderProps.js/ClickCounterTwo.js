6031613c189e0b979b6444ec0095ac22ee4cdeb7
atulmahajan08 <atulm875@gmail.com>
2022-01-20 23:29:04 +0530

    Http Request in react

--- src/components/RenderProps.js/ClickCounterTwo.js
+++ src/components/RenderProps.js/ClickCounterTwo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class ClickCounterTwo extends Component {
    
     render() {
-        const { count, incrementCount } = this.props
+        const { count, incrementCount } = this.props // destructuring
         return <button onClick={incrementCount}>Clicked {count} times</button>
     }
 }
