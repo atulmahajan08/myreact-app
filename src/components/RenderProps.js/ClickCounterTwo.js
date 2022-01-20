0d2e498745b3b166fa4ad04f1f0e2b686bb107a7
atulmahajan08 <atulm875@gmail.com>
2022-01-21 00:07:33 +0530

    destructring again

--- src/components/RenderProps.js/ClickCounterTwo.js
+++ src/components/RenderProps.js/ClickCounterTwo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class ClickCounterTwo extends Component {
    
     render() {
-        const { count, incrementCount } = this.props // destructuring
+        const { count, incrementCount } = this.props // destructuring from Props
         return <button onClick={incrementCount}>Clicked {count} times</button>
     }
 }
