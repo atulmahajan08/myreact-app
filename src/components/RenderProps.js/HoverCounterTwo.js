cfc2f7a1c4dedcb07e942fa702fb9b0ccfe42f50
atulmahajan08 <atulm875@gmail.com>
2022-01-21 00:20:26 +0530

    reverting changes

--- src/components/RenderProps.js/HoverCounterTwo.js
+++ src/components/RenderProps.js/HoverCounterTwo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class HoverCounterTwo extends Component {
    
     render() {
-        const { count, incrementCount } = this.props // destructuring props
+        const { count, incrementCount } = this.props // destructuring 
         return <h2 onMouseOver={incrementCount}>Hovered {count} times</h2>
     }
 }
